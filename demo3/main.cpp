701ec01ad9a52eb643f4ac01e42c7251187a9677
107061105 <yc95086@gmail.com>
2021-06-08 04:25:41 -0700

    third

--- demo3/main.cpp
+++ demo3/main.cpp
@@ -28,8 +28,6 @@ int main(){
             double dx = 0;
             double dz = 0;
             double degree = 0;
-            //double timed1 = d1 / 2 * 1000;
-            //double timed2 = (d2 - 3) / 2 * 1000;
         
             char recv[1];
 
@@ -74,7 +72,7 @@ int main(){
                     }
                 } while (recv[0] != '\r');
                 dz = d1 + d2;
-                //dz = dz * -1;
+                dz = dz * -1;
                 d1 = d2 = sign = order = 0;
 
                 do {
@@ -96,19 +94,50 @@ int main(){
                 } while (recv[0] != '\r');
                 degree = d1 + d2;
                 if (degree >= 270 && degree <= 360) degree = degree - 360;
-                d1 = d2 = sign = order = 0;
-                //printf("%f, %f, %f\r\n", dx, dz, degree);
 
-                if (abs(dz) <= 6 && abs(dx) <= 0.8 && (abs(degree) <= 8)) {
-                    car.stop();
-                } else if (abs(dz) <= 5 && abs(dx) <= 0.8 && (abs(degree) <= 10)) {
-                    car.stop();
-                } else if (abs(degree) <= 6) {
-                    if (abs(dx) > 0.2) {
+                if (abs(dz) <= 6) {
+                    if (abs(dx - degree / 6) <= 0.3) {
+                        car.stop();
+                        break;
+                    } else {
+                        if (dx - degree / 6 >= 1) car.follow(8, 8);
+                        else car.follow(-8, -8);
+                    }
+                } else {
+                    if (abs(dx) >= (abs(degree) / 4)) {
+                        if (dx < 0) {
+                            double speed = 10 * (abs(dx) - abs(degree) / 4) + 10 / (dz - 5);
+                            car.follow(30, -30 - speed);
+                        } else {
+                            double speed = 10 * (abs(dx) - abs(degree) / 4) + 10 / (dz - 5);
+                            car.follow(30 + speed, -30);
+                        }
+                    } else {
+                        car.goStraight(30);
+                    }
+                }
+
+                /*if (abs(dz) <= 6) {
+                    if (abs(dx) <= 0.2 && abs(degree) <= 6) {
+                        car.stop();
+                        break;
+                    } else if (abs(degree) <= 6){
                         if (dx > 0) car.follow(0, 8);
                         else car.follow(-8, 0);
-                    } else if ((abs(dz) > 8) && (abs(dx) <= 0.2)) {
+                    } else {
+                        double time1 = (90 - abs(degree)) / 6.5 * 100;
+                        double time2 = tan(abs(degree) * PI / 180) * 3500;
+                        if (degree > 0) car.follow(30, 30);
+                        else car.follow(-30, -30);
+                        ThisThread::sleep_for(time1);
+                        
                         car.goStraight(30);
+                        ThisThread::sleep_for(time2);
+                        if (degree > 0) car.follow(-30, -30);
+                        else car.follow(30, 30);
+                        ThisThread::sleep_for(1700ms);
+                        car.stop();
+                        break;
                     }
                 } else {
                     if (dx >= 0) {
@@ -116,23 +145,20 @@ int main(){
                             double speed = 6 * dx + 10 / dz + 1 * degree;
                             car.follow(30 + speed, -30);
                         } else {
-                            double speed = -6 * dx + 10 / dz + 1 * degree;
-                            car.follow(30, -30 - speed);
+                            double speed = 6 * dx + 10 / dz + 1 * degree;
+                            car.follow(30 + speed, -30);
                         }
                     } else {
                         if (degree > 0) {
                             double speed = -6 * dx + 5 / dz + 1 * degree;
                             car.follow(30, -30 - speed);
                         } else {
-                            double speed = 6 * dx + 5 / dz + 1 * degree;
-                            car.follow(30 + speed, -30);
+                            double speed = -6 * dx + 5 / dz + 1 * degree;
+                            car.follow(30, -30 - speed);
                         }
                     }
-                }
-
-                
+                }*/ 
             }
         }
     }
-
 }
